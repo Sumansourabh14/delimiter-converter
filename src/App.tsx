4511821c12e570481c3b1e6fd34a1c8da1e3600e
Suman Sourabh <sumsourabh14@gmail.com>
2025-06-17 01:11:40 +0530

    updated chakra components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 import { siteTitle } from "./data/content";
-import { Button, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  HStack,
+  Stack,
+  Switch,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
 
 function App() {
   const [text, setText] = useState("");
   const [output, setOutput] = useState("");
+  const [checked, setChecked] = useState(false);
 
   const spaceToHyphens = (input: string): string => {
     return input.replace(/ /g, "-");
@@ -17,41 +27,57 @@ function App() {
     setOutput(spaceToHyphens(text));
   };
 
+  const toggleLowerCase = () => {
+    if (output === output.toLowerCase()) {
+      setChecked(true);
+      setOutput(output.toUpperCase());
+    } else {
+      setChecked(false);
+      setOutput(output.toLowerCase());
+    }
+  };
+
   return (
     <>
       <h1>{siteTitle}</h1>
-      <p></p>
-
       <p>Convert space (" ") to hyphen ("-")</p>
-      <section>
+      <Container centerContent fluid>
         <form onSubmit={handleSubmit}>
-          <section>
-            <section>
-              <section>
+          <Box>
+            <HStack>
+              <Box>
                 <Textarea
                   style={{ fontFamily: "inherit" }}
                   placeholder="Enter text"
                   value={text}
                   onChange={(e) => setText(e.target.value)}
+                  variant={"subtle"}
+                  autoresize
                 />
-              </section>
-              <section>
+              </Box>
+              <Box>
                 <Textarea
                   style={{ fontFamily: "inherit" }}
                   placeholder="Output text"
                   value={output}
                   readOnly
+                  variant={"subtle"}
                 />
-              </section>
-            </section>
-            <section>
+              </Box>
+            </HStack>
+            <Box>
               <Button type="submit" disabled={text.trim().length === 0}>
-                Click me
+                Convert
               </Button>
-            </section>
-          </section>
+              <Switch.Root checked={checked} onCheckedChange={toggleLowerCase}>
+                <Switch.HiddenInput />
+                <Switch.Control />
+                <Switch.Label>Lower case</Switch.Label>
+              </Switch.Root>
+            </Box>
+          </Box>
         </form>
-      </section>
+      </Container>
     </>
   );
 }
